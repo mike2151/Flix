648db92f7b0e0c5a91868b6b5307e6956be3e982
Michael Abelar <miabelar@fb.com>
2018-06-27 08:01:33 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Michael Abelar on 6/27/18.
+//  Copyright © 2018 Michael Abelar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
